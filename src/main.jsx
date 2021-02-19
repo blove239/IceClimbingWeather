c53fad4a96c3cad8c57b384e385e7fbcbd6618c5
Brandon <blove239@gmail.com>
2021-02-19 18:27:29 -0500

    Initialized npm and created snowpack React dev environment

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+const App = () => {
+  return (
+    <h1>Hello World x2</h1>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
