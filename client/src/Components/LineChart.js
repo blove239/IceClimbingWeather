0a9810487f1983ce5cf6ea6aaed7b4ed600bd278
Brandon <blove239@gmail.com>
2021-03-15 18:17:27 -0400

    Added icons to chart x axes for clarity

--- client/src/Components/LineChart.js
+++ client/src/Components/LineChart.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Line } from 'react-chartjs-2'
+import React, { useEffect } from 'react'
+import { Chart, Line } from 'react-chartjs-2'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import day from '../images/day.png'
+import night from '../images/night.png'
 
 const LineChart = ({ hourlyWeather }) => {
     const data = {
@@ -27,22 +30,52 @@ const LineChart = ({ hourlyWeather }) => {
                     display: true,
                     labelString: 'Temperature °C'
                 },
-               
                 ticks: {
-                    callback: (value, index, values) => {
-                        return value + ' °C';
+                    callback: (value) => {
+                        return value;
                     }
                 }
             }],
             xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: "Date (M-DD-HH:MM)"
+                ticks: {
+                    padding: 40,
+                    fontSize: 0,
+                    callback: (value) => {
+                        if (value.endsWith('12:00')) {
+                            return 'day'
+                        } if (value.endsWith(' 0:00')) {
+                            return 'night'
+                        }
+                    }
                 }
             }]
         }
     }
-    
+
+    useEffect(() => {
+        return () => {
+            Chart.pluginService.register({
+                afterDraw: chart => {
+                    var ctx = chart.chart.ctx;
+                    var xAxis = chart.scales['x-axis-0'];
+                    var yAxis = chart.scales['y-axis-0'];
+                    xAxis.ticks.forEach((value, index) => {
+                        var x = xAxis.getPixelForTick(index);
+                        var image = new Image();
+                        if (value && value.endsWith('night')) {
+                            image.src = night
+                            ctx.drawImage(image, x - 15, yAxis.bottom + 10, 30, 30);
+                        } if (value && value.endsWith('day')) {
+                            image.src = day
+                            ctx.drawImage(image, x - 15, yAxis.bottom + 10, 30, 30);
+                        }
+                    });
+                }
+            });
+        }
+    })
+
+
     return (
         <Line
             data={data}
